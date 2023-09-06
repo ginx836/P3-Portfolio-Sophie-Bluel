a0c1d00551b1dcd99de736bfa1589ba5a0d13853
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-09-06 17:09:14 +0200

    Ajout de la section filtres
    Ajout de la section Filtres dans le fichier main.js
    Ajout des classes correspondantes dans le fichier style.css

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,6 +1,3 @@
-//Récupération des travaux depuis le fichier JSON
-const works = await fetch ('http://localhost:5678/api/works').then(works => works.json());
-
 /* 
 //Fonction qui génére le HTML des travaux
 function generateWorksHTML(works) {  //Prend en paramètre les travaux stockés dans la variable works
@@ -22,31 +19,28 @@ function generateWorksHTML(works) {  //Prend en paramètre les travaux stockés 
 generateWorksHTML(works); 
 */
 
- export function generateWorksHTML(works) {
+export function generateWorksHTML(works) {
   for (let i = 0; i < works.length; i++) {
     const work = works[i];
 
-  //Récupération de l'élément du DOM qui accueillera le travail courant
+    //Récupération de l'élément du DOM qui accueillera le travail courant
     const galleryElement = document.querySelector(".gallery");
 
-  //Création de l'élément qui accueillera le travail courant
+    //Création de l'élément qui accueillera le travail courant
     const figureElement = document.createElement("figure");
 
-  //Création des balises HTML qui accueilleront les informations du travail courant
+    //Création des balises HTML qui accueilleront les informations du travail courant
     const imageElement = document.createElement("img");
     imageElement.src = work.imageUrl;
 
     const figcaptionElement = document.createElement("figcaption");
     figcaptionElement.textContent = work.title;
 
-  //On rattache les balises HTML au travail courant
+    //On rattache les balises HTML au travail courant
     figureElement.appendChild(imageElement);
     figureElement.appendChild(figcaptionElement);
 
-  //On rattache le travail courant à la galerie
+    //On rattache le travail courant à la galerie
     galleryElement.appendChild(figureElement);
-    
   }
 }
-
-generateWorksHTML(works);
