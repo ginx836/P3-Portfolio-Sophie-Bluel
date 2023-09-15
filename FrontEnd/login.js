9c414ceb7193d35e637f22bd72bf949c416a695f
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-09-15 11:27:55 +0200

    Modification de la page login
    Modification de la gestion des messages d'erreur. Passage de la methode alert à un message inclus dans la page

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,12 +1,36 @@
-import { loginUser } from "./api.js"; // Importer la fonction loginUser depuis api.js
+import { loginUser } from "./api.js";
 
-document.getElementById("login").addEventListener("submit", handleLogin); // Ajouter un écouteur d'événement sur le formulaire de connexion
+document.getElementById("login").addEventListener("submit", handleLogin);
+
+/**
+ *
+ * @param {Event} event
+ */
 
 async function handleLogin(event) {
   event.preventDefault();
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
+  function errorMessage(message) {
+    const form = document.getElementById("loginForm");
+    const error = document.createElement("p");
+    error.textContent = message;
+    error.style.color = "red";
+    error.style.textAlign = "center";
+    error.style.marginBottom = "1em";
+    form.appendChild(error);
+
+    const submitButton = document.querySelector("input[type='submit']");
+    submitButton.addEventListener("click", function () {
+      error.remove();
+    });
+
+    setTimeout(function () {
+      error.remove();
+    }, 10000);
+  }
 
   try {
     // Essayer d'envoyer la requête à l'API
@@ -20,10 +44,10 @@ async function handleLogin(event) {
       window.location.href = "./index.html";
     } else {
       // Si la connexion échoue, afficher un message d'erreur
-      alert("Erreur dans l'identifiant ou le mot de passe");
+      errorMessage("Erreur dans l'identifiant ou le mot de passe");
     }
   } catch (error) {
-    // En cas d'erreur lors de la requête, afficher le message d'erreur
-    alert("Erreur lors de la requête : " + error.message);
+    // En cas d'erreur de connexion, afficher un message d'erreur
+    errorMessage("Impossible de se connecter au serveur");
   }
 }
